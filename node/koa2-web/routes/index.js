b2f3e58c02bec23190bce400755442fe336d101b
元宝 <shenxiaii@qq.com>
2019-01-24 15:18:42 +0800

    Update index.js
    添加生成验证码功能

--- node/koa2-web/routes/index.js
+++ node/koa2-web/routes/index.js
@@ -1,13 +1,32 @@
 const router = require('koa-router')()
+const svgCaptcha = require('svg-captcha')
 
 router.get('/', async (ctx) => {
   await ctx.render('index.html')
 })
 
 router.post('/addUser', async (ctx) => {
-  ctx.response.body = {data: {
+  ctx.body = {data: {
     id: '1111'
-  }, success: true};
+  }, success: true}
+})
+
+/**
+ * @desc {生成验证码图片}
+ * @type {string}
+ */
+let currentCodeText = ''
+router.post('/checkCode', async (ctx, next) => {
+  await next()
+  currentCodeText = svgCaptcha.create().text
+  ctx.body = svgCaptcha.create().data
+})
+
+router.post('/login', async (ctx, next) => {
+  await next()
+  console.log('params: ', ctx.params)
+  console.log('query: ', ctx.query)
+  ctx.body = {success: true}
 })
 
 module.exports = router
